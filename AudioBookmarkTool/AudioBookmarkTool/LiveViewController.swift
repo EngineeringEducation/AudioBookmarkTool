ffccbe644422394d04d2c23b962351158615eaa6
Emma Koszinowski <emkosz@gmail.com>
2015-05-25 21:12:32 -0700

    TableView for questions
    Added a table view that loads questions from 
    an array.

--- AudioBookmarkTool/AudioBookmarkTool/LiveViewController.swift
+++ AudioBookmarkTool/AudioBookmarkTool/LiveViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 
-class LiveViewController: UIViewController {
+class LiveViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     
     @IBOutlet weak var startButton: UIButton!
@@ -24,13 +24,48 @@ class LiveViewController: UIViewController {
     var elapsed_time: NSTimeInterval = 0
     
     
+    //label to show the elapsed time
     @IBOutlet weak var elapsedTimeLabel: UILabel!
+    
+    
+    // table view for the questions
+    @IBOutlet
+    var questionsTableView: UITableView!
+    
+    var questions: [String] = [
+        "What are your thougts on bla",
+        "How would you describe bla",
+        "Given that bla what's the next bla",
+        "What would be the best bla",
+        "And to conclude, bla bla bla"
+    ]
+    
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.questionsTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.questions.count;
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:UITableViewCell = self.questionsTableView.dequeueReusableCellWithIdentifier("cell") as! UITableViewCell
+        
+        cell.textLabel?.text = self.questions[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        println("You selected cell #\(indexPath.row)!")
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -60,8 +95,13 @@ class LiveViewController: UIViewController {
         println("Time elapsed: \(elapsed_time)")
         elapsedTimeLabel.text = "\(elapsed_time) seconds"
     }
-
+    
+    
 }
+    
+    
+
+
 
 
 
