7620f4c9a03d1a87c5bea258ee87355a5ee78c12
Emma Koszinowski <emkosz@gmail.com>
2015-05-25 17:55:19 -0700

    calculate event time range

--- AudioBookmarkTool/AudioBookmarkTool/LiveViewController.swift
+++ AudioBookmarkTool/AudioBookmarkTool/LiveViewController.swift
@@ -13,9 +13,18 @@ class LiveViewController: UIViewController {
     
     
     @IBOutlet weak var startButton: UIButton!
-    startButton.enable = false
+    var currentStartTime: NSDate? // gives an NSDate instance that represents now
+    //var startTimer: Bool
+    //startTimer.enable = false    //the start button should only be pressable once, only this code doesnt do that..
     
     
+    @IBOutlet weak var stopButton: UIButton!
+    var currentStopTime: NSDate? // also gives and NSDate instance that represents now
+    
+    var elapsed_time: NSTimeInterval = 0
+    
+    
+    @IBOutlet weak var elapsedTimeLabel: UILabel!
     
 
     override func viewDidLoad() {
@@ -30,22 +39,27 @@ class LiveViewController: UIViewController {
     
     
     
-    @IBAction func startTimer(sender: AnyObject) {
+    @IBAction func startTimer(sender: UIButton) {
         println("Start button pressed!")
-        var currentTime = NSDate() // ger en NSDate instans som representerar nu
-        println("Current time is \(currentTime)")
-        
-        
-        
+        currentStartTime = NSDate()
+        println("Current start time is \(currentStartTime!)")
+       
     }
     
     
-    
-    @IBAction func stopTimer(sender: AnyObject) {
+    @IBAction func stopTimer(sender: UIButton) {
         println("Stop button pressed!")
+        currentStopTime = NSDate()
+        println("Current stop time is \(currentStopTime!)")
+        eventRange()
     }
     
-
+    // Calculates the elapsed time from start to stop
+    func eventRange() {
+        elapsed_time = currentStopTime!.timeIntervalSince1970 - currentStartTime!.timeIntervalSince1970
+        println("Time elapsed: \(elapsed_time)")
+        elapsedTimeLabel.text = "\(elapsed_time) seconds"
+    }
 
 }
 
